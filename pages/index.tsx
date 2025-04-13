4f2896f85a0371ce0fa235d166dd8a2226231369
tamasma <abdessamad87@hotmail.es>
2025-04-13 21:19:54 +0200

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,10 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
+sdfsdfsd
+
 export default function Home() {
+  sdfsdfdsf
   return (
     <div className={styles.container}>
       <Head>
